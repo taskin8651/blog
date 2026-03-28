8238d328e5a6f6bad0d817c0b0841ce1291ffc0b
taskin8651 <mdsayebalam10@gmail.com>
2026-03-28 11:23:27 +0530

    ds

--- resources/views/custom/master.blade.php
+++ resources/views/custom/master.blade.php
@@ -68,7 +68,7 @@
 
 <li class="{{ request()->is('password.edit*') ? 'active' : '' }}">
     <a href="{{ url('profile/password') }}">
-        <i class="ti ti-user"></i>
+        <i class="ti ti-user"></i> 
     </a>
 </li>
 
