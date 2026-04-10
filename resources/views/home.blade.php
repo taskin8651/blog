1c1b78a38f2a74c8657ec15b19c5bfd571356e2e
Sanket Kumar <ssbkumar5513@gmail.com>
2026-04-10 22:59:53 +0530

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,6 +20,31 @@
             </div>
         </div>
     </div>
+    <div class="flex flex-wrap items-center md:justify-between justify-center">
+
+            <!-- Left Text -->
+            <div class="w-full md:w-4/12 px-4">
+                <div class="text-sm text-blueGray-500 font-semibold py-1 text-center md:text-left">
+                    © {{ date('Y') }} Developed by
+                    <a href="https://sanketkumarofficial.com"
+                       class="font-semibold"
+                       style="color:#DB2777; transition:0.2s;"
+                       onmouseover="this.style.color='#BE185D'"
+                       onmouseout="this.style.color='#DB2777'"
+                       target="_blank">
+                        Sanket Kumar
+                    </a>.
+                </div>
+            </div>
+
+            <!-- Hidden Links Removed / Cleaned -->
+            <div class="w-full md:w-8/12 px-4">
+                <ul class="flex flex-wrap list-none md:justify-end justify-center hidden">
+                    <!-- Empty because you want a clean footer -->
+                </ul>
+            </div>
+
+        </div>
 </div>
 @endsection
 @section('scripts')
