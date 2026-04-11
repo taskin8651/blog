a9c5f810bd126739867ad460146f898960190b96
Sanket Kumar <ssbkumar5513@gmail.com>
2026-04-11 14:12:09 +0530

    sanket

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -200,7 +200,7 @@
 
     </nav>
 
-    {{-- LOGOUT --}}
+    {{-- LLLOGOUT --}}
     <div class="border-t border-slate-700 p-3">
         <a href="#"
            onclick="event.preventDefault(); document.getElementById('logoutform').submit();"
